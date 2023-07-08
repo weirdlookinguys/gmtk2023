b6d0c47db01440174418155ec42b8165e80589d9
Angus Goucher <gus.goucher@gmail.com>
2023-07-08 13:49:38 -0400

    added my tile assets and the map importer plugin

---
+++ Assets/Maps/reference.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="reference" tilewidth="63" tileheight="64" tilecount="1" columns="1">
+ <image source="reference.png" width="63" height="64"/>
+</tileset>
