fb88f94a97edc5c8226fdf492bf60f76a5e0f9cb
Santos Lopez-Lucas <69735829+slopezlucas@users.noreply.github.com>
2023-07-07 21:07:10 -0400

    Added dialogue class (skeleton nothing in it really).

---
+++ Assets/dialogue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    private Dictionary<string, string[,]> DialogueLibrary = new Dictionary<string, string[,]>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
